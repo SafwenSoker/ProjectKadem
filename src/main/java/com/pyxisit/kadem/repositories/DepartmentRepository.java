3a1fac0b4bc3f6866f536c488d2e678805fb2c81
Safwen Soker <safwen.soker@pyxis.com.tn>
2023-03-26 03:22:03 +0100

    Dev/Feat: Add function to departments by Universite

--- src/main/java/com/pyxisit/kadem/repositories/DepartmentRepository.java
+++ src/main/java/com/pyxisit/kadem/repositories/DepartmentRepository.java
@@ -3,6 +3,9 @@ package com.pyxisit.kadem.repositories;
 import com.pyxisit.kadem.entities.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DepartmentRepository extends JpaRepository<Department, Integer> {
 
+    public List<Department> getDepartementsByUniversite(Integer idUniversite);
 }
