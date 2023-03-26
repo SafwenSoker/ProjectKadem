1aca5caec7793f1d973ca462210175f205629478
Safwen Soker <safwen.soker@pyxis.com.tn>
2023-03-26 03:21:12 +0100

    Dev/Feat: Add getters, setters and constructors to Universite entity

--- src/main/java/com/pyxisit/kadem/entities/Universite.java
+++ src/main/java/com/pyxisit/kadem/entities/Universite.java
@@ -1,9 +1,16 @@
 package com.pyxisit.kadem.entities;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
+@Setter
+@Getter
+@NoArgsConstructor
 public class Universite{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
