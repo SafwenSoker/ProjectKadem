4c6a358a80e686699a70dab9cd7f391eadb4364b
Safwen Soker <safwen.soker@pyxis.com.tn>
2023-03-26 03:20:39 +0100

    Dev/Feat: Add two functions - assignUniversiteToDepartment - getMontantContratEntreDeuxDate

--- src/main/java/com/pyxisit/kadem/services/IUniversiteServices.java
+++ src/main/java/com/pyxisit/kadem/services/IUniversiteServices.java
@@ -2,11 +2,17 @@ package com.pyxisit.kadem.services;
 
 import com.pyxisit.kadem.entities.Universite;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 public interface IUniversiteServices {
     List<Universite> retrieveAllUniversites();
     Universite addUniversite (Universite u);
     Universite updateUniversite (Universite u);
     Universite retrieveUniversite (Integer idUniversite);
+
+    public void assignUniversiteToDepartment(Integer idUniversite, Integer idDepartment);
+
+    public Map<String, Float> getMontantContratEntreDeuxDate(int idUniv, Date startDate, Date endDate);
 }
