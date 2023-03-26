5ca0765c052d4d984085ae930ccc86e1a3c51d52
Safwen Soker <safwen.soker@pyxis.com.tn>
2023-03-26 03:11:18 +0100

    Dev/Feat: Add function to get equipes by etudiant option and etudiants contrats specialité

--- src/main/java/com/pyxisit/kadem/repositories/EquipeRepository.java
+++ src/main/java/com/pyxisit/kadem/repositories/EquipeRepository.java
@@ -1,7 +1,11 @@
 package com.pyxisit.kadem.repositories;
 
 import com.pyxisit.kadem.entities.Equipe;
+import com.pyxisit.kadem.entities.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EquipeRepository extends JpaRepository<Equipe, Integer> {
+    List<Equipe> findByEtudiantsOptAndEtudiantsContratsSpecialite(Option op);
 }
