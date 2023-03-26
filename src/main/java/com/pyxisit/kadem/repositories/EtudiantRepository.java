26966441794ec1d7e518dda8fe8b08d0288efe74
Safwen Soker <safwen.soker@pyxis.com.tn>
2023-03-26 03:12:01 +0100

    Dev/Feat: Add function to get etudiant by nome and prenome

--- src/main/java/com/pyxisit/kadem/repositories/EtudiantRepository.java
+++ src/main/java/com/pyxisit/kadem/repositories/EtudiantRepository.java
@@ -3,7 +3,10 @@ package com.pyxisit.kadem.repositories;
 
 import com.pyxisit.kadem.entities.Etudiant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EtudiantRepository extends JpaRepository<Etudiant, Integer> {
-
+    Etudiant findByNomEAndPrenomE(String nom, String prenom);
 }
